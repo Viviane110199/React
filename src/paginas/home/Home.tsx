aedfaefa4ce14b421ead09a5ff673b87e83a193e
Viviane110199 <vivianerego22@gmail.com>
2022-04-26 00:04:44 -0300

    Subindo arquivos

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css';
+
+function Home() {
+    return(
+        <>
+        <h1 className="titulo">Vivi</h1>
+        <img src="https://i.imgur.com/1XY5u0R.jpeg" alt="Imagem de uma flor" className="img"/>
+        </>
+    );
+}
+
+export default Home;
